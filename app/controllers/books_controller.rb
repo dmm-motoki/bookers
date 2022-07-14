fc3e10348d655efe611a43bc790fba3e41b7c32f
motoki3150 <108526062+motoki3150@users.noreply.github.com>
2022-07-14 14:04:14 +0000

    [fin]bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(list_params)
+    @book = Book.new(book_params)
     if @book.save
       redirect_to book_path(@book.id)
     else
@@ -13,11 +13,26 @@ class BooksController < ApplicationController
   end
 
   def index
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
