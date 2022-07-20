09b0938dcd5c93a390b18b6ba789336739e376bd
motoki3150 <108526062+motoki3150@users.noreply.github.com>
2022-07-20 13:16:05 +0000

    [fin]bookers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_14_120301) do
+ActiveRecord::Schema.define(version: 2022_07_20_115129) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
